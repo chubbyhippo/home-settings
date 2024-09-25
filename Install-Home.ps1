9e61fbef0525717fd3ad5909f0569b33394abba7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-09-25 12:18:42 +0700

    Update Install-Home.ps1

--- Install-Home.ps1
+++ Install-Home.ps1
@@ -1,2 +1,1 @@
-# Set the HOME environment variable to the value of the $HOME PowerShell variable
 [System.Environment]::SetEnvironmentVariable("HOME", $HOME, [System.EnvironmentVariableTarget]::User)
